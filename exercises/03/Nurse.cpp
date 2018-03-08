e0f1cb13c20da443f6262d0d5e5d790e86bc8d79
Stilian Goranov <33035110+stiliangoranov@users.noreply.github.com>
2018-03-08 22:25:45 +0200

    Update Nurse.cpp

--- exercises/03/Nurse.cpp
+++ exercises/03/Nurse.cpp
@@ -1,16 +1,37 @@
 #include "Nurse.h"
-
+#include "Patient.h"
 #include <iostream>
-
-Nurse::Nurse()
-{
-	setName("Gergana");
-}
 
 void Nurse::setName(char const* _name)
 {
-	if (strlen(_name) >= 50)
-		std::cout << "Too much length in name!\n";
+	int len = strlen(_name) + 1;
+	if (len < 50)
+		strcpy_s(this->name, len, _name);
 	else
-		strcpy_s(name, strlen(_name) + 1, _name);
+		std::cout << "Too much length!";
+}
+
+void Nurse::setYearsOfWork(int _yearsOfWork)
+{
+	if (_yearsOfWork >= 0 && _yearsOfWork <= 100)
+		this->yearsOfWork = _yearsOfWork;
+	else
+		std::cout << "Invalid years of work!";
+}
+
+Nurse::Nurse(char const* _name, int _yearsOfWork)
+{
+	setName(_name);
+	setYearsOfWork(_yearsOfWork);
+}
+
+void Nurse::print() const
+{
+	std::cout << "Nurse's name: " << this->name << '\n';
+	std::cout << "Years of work in hospital: " << this->yearsOfWork << '\n';
+}
+
+void Nurse::help(Patient const& patient) const
+{
+	std::cout << "Nurse " << this->name << " helped patient " << patient.getName() << " sucessfully!\n";
 }
