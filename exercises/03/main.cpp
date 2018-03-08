74cf648b6aa978e58af8be7cdc5b551733be61d8
Stilian Goranov <33035110+stiliangoranov@users.noreply.github.com>
2018-03-08 22:28:17 +0200

    Update main.cpp

--- exercises/03/main.cpp
+++ exercises/03/main.cpp
@@ -1,11 +1,38 @@
-#include "Patient.h"
-#include <iostream>
+#include "Hospital.h"
+
+void testHospital()
+{
+	// initializing nurses and patients using constructor with parameters
+	Nurse n1("Galena", 3);
+	Nurse n2("Nikoleta", 1);
+
+	Patient p1("Georgi Ivanov Gonzo", 30, "Svruh doza narkotici", 3, &n1);
+	Patient p2("Hristo Stoichkov", 50, "Mnogo psuvane", 5, &n2);
+
+	
+	Hospital h("Sofia MED", true);
+	h.addNurse(n1);
+	h.addNurse(n2);
+	h.addPatient(p1);
+	h.addPatient(p2);
+
+	h.print();
+
+	p1.callNurse();
+	n1.help(p1);
+
+	p2.callNurse();
+	n2.help(p2);
+
+	h.removeNurse(n2);
+	h.removePatient(p1);
+
+	h.print();
+}
 
 int main()
 {
-	Patient p;
+	testHospital();
 
-	p.print();
-
-	p.callNurse();
+	return 0;
 }
