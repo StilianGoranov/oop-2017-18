61b916592b83c2107de2a451ec58a170e2887954
Stilian Goranov <33035110+stiliangoranov@users.noreply.github.com>
2018-06-10 10:04:42 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "Laboratory.h"
+
+
+void laboratoryDemo()
+{
+    Laboratory lab;
+    
+    PackageStudy packageStudy("packagestudy1", -3, 5);
+    packageStudy.addStudy(SingleStudy("pstudy1", 500, 18, -300, 1000));
+    packageStudy.addStudy(SingleStudy("pstudy2", 300, 1, -400, 700));
+
+    lab.addStudy(SingleStudy("study1", 50, 20, -30, 100));
+    lab.addStudy(SingleStudy("study2", 1000, 100, -500, 2000));
+    lab.addStudy(packageStudy);
+    
+    std::cout << lab.getFastestExecutableStudy()->getName() << std::endl;
+    std::cout << lab.getStudiesPrice() << std::endl;
+}
+
+
+int main()
+{
+    laboratoryDemo();
+    
+    return 0;
+}
